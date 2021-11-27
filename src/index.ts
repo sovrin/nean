e6cb5660594564a505a0fbdc51d85c9b097ddfdb
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2021-11-27 11:22:45 +0100

    fix, rearrange exports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import factory from './factory';
-import createHook, {interceptHook} from './hook';
-import {useType} from './hooks';
-
 /**
  * User: Oleg Kamlowski <oleg.kamlowski@thomann.de>
  * Date: 23.09.2019
  * Time: 21:14
  */
-export default factory;
-export {
-    interceptHook,
-    createHook,
-    useType,
-};
+export {default} from './factory';
+export {useType} from './hooks';
+export {interceptHook, default as createHook} from './hook';
