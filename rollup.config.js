18efc06c68a3e068b392e7b81a20f5a80038d89f
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2022-05-18 13:57:54 +0200

    feat, update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,41 @@
-import pkg from './package.json';
 import typescript from '@rollup/plugin-typescript';
-import commonJS from '@rollup/plugin-commonjs';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
+import dts from 'rollup-plugin-dts';
 
-const peerDependencies = Object.keys(pkg.peerDependencies || {});
+const pkg = require('./package.json');
+const external = Object.keys(pkg.peerDependencies || {});
 
 /**
  * User: Oleg Kamlowski <oleg.kamlowski@thomann.de>
  * Date: 19.10.2019
  * Time: 13:43
  */
-export default {
-    input: 'src/index.ts',
-    output: {
-        name: 'nean',
-        format: 'es',
-        file: pkg.main,
+export default [
+    {
+        input: 'src/index.ts',
+        output: {
+            name: 'nean',
+            format: 'es',
+            file: pkg.main,
+        },
+        external,
+        plugins: [
+            typescript({
+                tsconfig: './tsconfig.prod.json',
+            }),
+            nodeResolve(),
+        ],
     },
-    external: [
-        ...peerDependencies,
-    ],
-    plugins: [
-        typescript({
-            tsconfig: './tsconfig.prod.json',
-        }),
-        nodeResolve(),
-        commonJS(),
-    ],
-};
+    {
+        input: 'dist/index.d.ts',
+        output: [
+            {
+                file: 'types/index.d.ts',
+                format: 'esm',
+            },
+        ],
+        plugins: [
+            dts()
+        ],
+    },
+];
