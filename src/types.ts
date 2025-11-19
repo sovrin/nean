30d3585cbab8a5fa84d234586a74032258d3fdd7
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2025-11-19 21:50:29 +0100

    fix(types): wip

--- src/types.ts
+++ src/types.ts
@@ -1,48 +1,53 @@
-import type {
-    ForwardRefExoticComponent,
-    JSX,
-    PropsWithChildren,
-    ReactElement,
-} from "react";
+import { JSX, PropsWithChildren, ReactElement, ReactNode } from "react";
 import type { Resolver } from "./resolver";
 import type { Hook } from "./hook";
 
 export type nean = (resolver?: Resolver) => Factory;
 
-export type Config<P, T extends ElementList> = {
-    as?: T;
+export type Config<Props extends object, Type extends ElementList> = {
+    as?: Type;
     className?: string;
-    style?: (props: ComponentProps<P, T>) => any;
-    extend?: (props: ComponentProps<P, T>) => any;
+    style?: (props: ComponentProps<Props, Type>) => any;
+    extend?: (props: ComponentProps<Props, Type>) => any;
     render?: (
-        props: ComponentProps<P, T>,
+        props: ComponentProps<Props, Type>,
         hooks?: { [key: string]: Function },
-    ) => ReactElement | null;
+    ) => ReactNode;
 };
 
-export type Factory = <P, T extends ElementList>(
-    config: Config<P, T>,
-) => ForwardRefExoticComponent<Prettify<ComponentProps<P, T>>>;
+export type Factory = <
+    Props extends object = never,
+    Type extends ElementList = never,
+    Overwrite extends Type = Type,
+>(
+    config: Config<Props, Type>,
+) => Renderer<Props, Type, Overwrite>;
 
-type ComponentProps<P, T extends ElementList> = PropsWithChildren<
-    P & ConsumerProps & ElementProps<T>
->;
+type Renderer<
+    Props extends object,
+    Type extends ElementList = never,
+    Overwrite extends Type = Type,
+> = {
+    <Generic extends ElementList = Overwrite>(
+        props: [Generic] extends [never]
+            ? FrameworkProps<never> & Props
+            : FrameworkProps<Generic> & ElementProps<Generic> & Props,
+    ): ReactElement | null;
 
-type ElementList = keyof JSX.IntrinsicElements;
+    <Fallback extends ElementList = Overwrite>(
+        props: ElementProps<Fallback> & Props,
+    ): ReactElement | null;
+};
 
+type ComponentProps<
+    Props extends object,
+    Type extends ElementList,
+> = PropsWithChildren<Props & FrameworkProps<Type> & ElementProps<Type>>;
+
+export type FrameworkProps<T extends ElementList> = {
+    as?: T;
+    use?: Hook[];
+};
+
+export type ElementList = keyof JSX.IntrinsicElements;
 type ElementProps<T extends ElementList> = JSX.IntrinsicElements[T];
-
-export type ConsumerProps = {
-    className?: string;
-    use?: Hook[];
-};
-
-export type FrameworkProps = {
-    as?: ElementList;
-    className?: string;
-    use?: Hook[];
-};
-
-export type Prettify<T> = {
-    [K in keyof T]: T[K];
-} & {};
