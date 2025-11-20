2236a18084be24e9abb185a1ae4d895bac17e539
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2025-11-20 21:32:07 +0100

    fix(lib): exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@
  * Date: 23.09.2019
  * Time: 21:14
  */
-export { default, type nean, type Config } from "./nean";
+export { default, type CreateFactory as nean, type ComponentConfig as Config } from "./nean";
 export { interceptHook, createHook, type Hook } from "./hook";
 export { resolver, type Resolver, type ResolverValue } from "./resolver";
